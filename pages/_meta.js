0374649e56f5fa0e305c4ba3e8f271a1d6b0e528
mahek <mahekzehra5@gmail.com>
2025-10-14 02:40:43 -0700

    project setup

---
+++ pages/_meta.js
@@ -0,0 +1,15 @@
+export default {
+  index: "Introduction",
+
+  contact: {
+    title: "Contact ↗",
+    type: "page",
+    href: "https://www.gripo.io/contact",
+    newWindow: true,
+  },
+  "request-demo": {
+    title: "Request free license 🔥",
+    type: "page",
+    href: "https://www.gripo.io/request-free-license",
+  },
+};
